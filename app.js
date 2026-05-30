e0981a6a7460dcadfc1aed9ac258695b51a56564
FelmArch <ferrlinda.dl@gmail.com>
2026-05-30 13:13:03 +0200

    feat: implementar mudanca dinamica do tamanho da grade no clique

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
 const board = document.querySelector("#container");
-const botao = document.querySelector("#btnGrid");
+const botaoTamanho = document.querySelector("#btnGrid");
 
 for (let i = 0; i < 256 ; i++) {
     const novoQuadro = document.createElement("div");
@@ -13,6 +13,23 @@ for (let i = 0; i < 256 ; i++) {
     });
 };
 
-btnGrid.addEventListener("click", () => {
+botaoTamanho.addEventListener("click", () => {
     let tamanhoNovo = prompt("Porfavor escolha um tamanho desejado!"); 
-})
+    tamanhoNovo = parseInt(tamanhoNovo);
+    
+    if (tamanhoNovo > 100) {
+        alert("Invalido! O máximo permitido é 100.")
+    } else {
+        board.innerHTML = "";
+
+        for (let i = 0; i < tamanhoNovo * tamanhoNovo; i++) {
+            const novoQuadro = document.createElement("div");
+            novoQuadro.classList.add("pixel");
+            board.appendChild(novoQuadro);
+
+            novoQuadro.addEventListener("mouseover", () => {
+                novoQuadro.style.backgroundColor = "#3498db";
+            });
+        }
+    }
+});

d3731e48e6a212db600c8f7a894f51bb820ea13c
FelmArch <ferrlinda.dl@gmail.com>
2026-05-30 18:29:55 +0200

    refactor: isolar a criacao do tabuleiro em funcao reutilizavel

--- app.js
+++ app.js
@@ -3,22 +3,29 @@
 const board = document.querySelector("#container");
 const botaoTamanho = document.querySelector("#btnGrid");
 
-for (let i = 0; i < 256 ; i++) {
-    const novoQuadro = document.createElement("div");
-    novoQuadro.classList.add("pixel");
-    const tamanhoInicial = 800 / 16;
-    novoQuadro.style.width = `${tamanhoInicial}px`;
-    novoQuadro.style.height = `${tamanhoInicial}px`;
-    novoQuadro.style.opacity = "0";
-    board.appendChild(novoQuadro);
 
-    novoQuadro.addEventListener("mouseover", () => {
-        let opacidadeAtual = parseFloat(novoQuadro.style.opacity);
-        if (opacidadeAtual < 1) {
-            novoQuadro.style.opacity = (opacidadeAtual + 0.1).toString();
-        }
-    });
-};
+function criarTabuleiro(tamanho) {
+    const tamanhoQuadrado = 800 / tamanho;
+    const totalQuadrados = tamanho * tamanho;
+
+    for (let i = 0; i < totalQuadrados; i++) {
+        const novoQuadro = document.createElement("div");
+        novoQuadro.classList.add("pixel");
+        novoQuadro.style.width = `${tamanhoQuadrado}px`;
+        novoQuadro.style.height = `${tamanhoQuadrado}px`;
+        novoQuadro.style.opacity = "0";
+        board.appendChild(novoQuadro);
+        
+        novoQuadro.addEventListener("mouseover", () => {
+            let opacidadeAtual = parseFloat(novoQuadro.style.opacity);
+            if(opacidadeAtual < 1) {
+                novoQuadro.style.opacity = (opacidadeAtual + 0.1).toString();
+            }
+        });
+    }
+}
+
+criarTabuleiro(16);
 
 botaoTamanho.addEventListener("click", () => {
     let tamanhoNovo = prompt("Porfavor escolha um tamanho desejado!"); 
@@ -26,24 +33,8 @@ botaoTamanho.addEventListener("click", () => {
     
     if (tamanhoNovo > 100) {
         alert("Invalido! O máximo permitido é 100.")
-    } else {
+    } else if (tamanhoNovo > 0) {
         board.innerHTML = "";
-
-        for (let i = 0; i < tamanhoNovo * tamanhoNovo; i++) {
-            const novoQuadro = document.createElement("div");
-            novoQuadro.classList.add("pixel");
-            const tamanhoQuadrado = 800 / tamanhoNovo;
-            novoQuadro.style.width = `${tamanhoQuadrado}px`;
-            novoQuadro.style.height = `${tamanhoQuadrado}px`;
-            novoQuadro.style.opacity = "0";
-            board.appendChild(novoQuadro);
-
-            novoQuadro.addEventListener("mouseover", () => {
-                let opacidadeAtual = parseFloat(novoQuadro.style.opacity);
-                if (opacidadeAtual < 1) {
-                    novoQuadro.style.opacity = (opacidadeAtual + 0.1).toString();
-                }
-            });
-        }
+        criarTabuleiro(tamanhoNovo);
     }
 });

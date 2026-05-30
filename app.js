c674d2e23edd24009c0e2eec7eb6ec8a9f8ddf1c
FelmArch <ferrlinda.dl@gmail.com>
2026-05-30 12:23:43 +0200

    feat: estruturar botao de tamanho no html e capturar clique no js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 
 const board = document.querySelector("#container");
+const botao = document.querySelector("#btnGrid");
 
 for (let i = 0; i < 256 ; i++) {
     const novoQuadro = document.createElement("div");
@@ -11,3 +12,7 @@ for (let i = 0; i < 256 ; i++) {
         novoQuadro.style.backgroundColor = "#3498db";
     });
 };
+
+btnGrid.addEventListener("click", () => {
+    let tamanhoNovo = prompt("Porfavor escolha um tamanho desejado!"); 
+})
